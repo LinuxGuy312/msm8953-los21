8c4b16a32012b8e1b09399ff6282e963f3e4bf09
Azure-Helper <azurehelper219@gmail.com>
2023-05-12 08:54:38 +0000

    Add drivers/scsi/ modifications
    Signed-off-by: LinuxGuy312 <githubwh@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -3453,7 +3453,7 @@ static int ufshcd_exec_dev_cmd(struct ufs_hba *hba,
 {
 	struct ufshcd_lrb *lrbp;
 	int err;
-	int tag;
+	int tag = 0;
 	struct completion wait;
 	unsigned long flags;
 
@@ -6942,7 +6942,7 @@ static int ufshcd_issue_tm_cmd(struct ufs_hba *hba, int lun_id, int task_id,
 	struct utp_upiu_task_req *task_req_upiup;
 	struct Scsi_Host *host;
 	unsigned long flags;
-	int free_slot;
+	int free_slot = 0;
 	int err;
 	int task_tag;
 
