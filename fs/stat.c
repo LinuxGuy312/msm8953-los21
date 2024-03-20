4b41f4e48e5ccba2f39699697fe165c455245856
Kartatz <105828205+Kartatz@users.noreply.github.com>
2024-03-20 19:38:54 +0800

    fs: suspicious-fs: prevent detection through common methods
    Prevent Zygisk/KernelSU detection through common methods
    
    This prevents the kernel from revealing KernelSU/Zygisk mount points in /proc
    for non-system apps and also prevents scanning the filesystem for suspicious
    files and directories.
    
    Signed-off-by: Kartatz <105828205+Kartatz@users.noreply.github.com>
    
    from:
    https://github.com/Dominium-Apum/kernel_xiaomi_chime/pull/1
    https://github.com/Dominium-Apum/kernel_xiaomi_chime/pull/3
    
    + guard everything properly
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- fs/stat.c
+++ fs/stat.c
@@ -14,6 +14,9 @@
 #include <linux/security.h>
 #include <linux/syscalls.h>
 #include <linux/pagemap.h>
+#ifdef CONFIG_SUS_FS
+#include <linux/suspicious.h>
+#endif
 
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
@@ -65,6 +68,12 @@ EXPORT_SYMBOL(vfs_getattr_nosec);
 int vfs_getattr(struct path *path, struct kstat *stat)
 {
 	int retval;
+
+#ifdef CONFIG_SUS_FS
+	if (is_suspicious_path(path)) {
+		return -ENOENT;
+	}
+#endif	
 
 	retval = security_inode_getattr(path);
 	if (retval)
