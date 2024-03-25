52e96fc9ea98790b6df56ebbebb644f8fa8157e5
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-03-25 13:35:40 +0800

    KernelSU: update to 11708
    import from https://github.com/backslashxx/KernelSU/tree/11708
    import kernel folder only

--- kernel_builder_clang.sh
+++ kernel_builder_clang.sh
@@ -21,7 +21,7 @@ export ARCH=arm64
 export SUBARCH=arm64
 export HEADER_ARCH=arm64
 
-KSUVER="11657"
+KSUVER="11708"
 
 if [ ! -d "KernelSU" ]; then
   git clone https://github.com/backslashxx/KernelSU -b $KSUVER
